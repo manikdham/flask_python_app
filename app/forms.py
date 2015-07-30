e024dd1f97de7878672ad032005df007375005ef
root <root@ip-10-42-104-164.(none)>
2015-07-30 10:14:55 +0000

    added css and some extras

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,23 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, TextAreaField, SubmitField, RadioField,validators
+from wtforms import StringField, TextAreaField, SubmitField, RadioField,validators
  
 class UserForm(Form):
-  username = TextField("Username",[validators.Required()])
-  password = TextField("Password",[validators.Required()])
-  homefolder = TextField("HomeFolder",[validators.Required()])
-  shelltype = TextField("ShellType",[validators.Required()])
+  username = StringField("Username",[validators.DataRequired()])
+  password = StringField("Password",[validators.DataRequired()])
+  homefolder = StringField("HomeFolder",[validators.DataRequired()])
+  shelltype = StringField("ShellType",[validators.DataRequired()])
   submit = SubmitField("Submit")
 
 class ModifyForm(Form):
-  value1 = TextField("Value1")
-  value2 = TextField("Value2")
-  value3 = TextField("Value3")
-  value4 = TextField("Value4")
+  value1 = StringField("Value1")
+  value2 = StringField("Value2")
+  value3 = StringField("Value3")
+  value4 = StringField("Value4")
   options = RadioField("Options1", choices=[('username','USERNAME'),('homedir','HOME DIR'),('password','PASSWORD')])
   submit1 = SubmitField("Submit1")
 
 class SudoForm(Form):
-  username = TextField("Username",[validators.Required()])
+  username = StringField("Username",[validators.DataRequired()])
   sudo_options = RadioField('Options', choices=[('yes','YES'),('no','NO')])
   submit = SubmitField("Submit")
 
